6289136b60e02b9f1d79630b672f2156b596281f
Aayu1254 <aayu27aas@gmail.com>
2023-07-31 16:46:56 +0530

    modified:   .github/workflows/build-arcturus.yml 	new file:   env-setup.sh 	new file:   kernelbuild.sh 	new file:   kernelbuild.sh

---
+++ kernelbuild.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Copyright (C) 2020-2021 Adithya R.
+
+SECONDS=0 # builtin bash timer
+ZIPNAME="Arcturus-X3-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="$GITHUB_WORKSPACE/proton-clang"
+DEFCONFIG="x3_defconfig"
+
+export PATH="$TC_DIR/bin:$PATH"
+
+if ! [ -d "$TC_DIR" ]; then
+	echo "clang-proton not found! Cloning to $TC_DIR..."
+	if ! git clone https://github.com/akmacc/clang-r487747c "$TC_DIR"; then
+		echo "Cloning failed! Aborting..."
+		exit 1
+	fi
+fi
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+	exit
+fi
+
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+	rm -rf out
+fi
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz dtbo.img CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log
+
+kernel="out/arch/arm64/boot/Image.gz"
+dtb="out/arch/arm64/boot/dts/qcom/sm8150-v2.dtb"
+dtbo="out/arch/arm64/boot/dtbo.img"
+
+if [ -f "$kernel" ] && [ -f "$dtb" ] && [ -f "$dtbo" ]; then
+	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+	if ! git clone -q https://github.com/Aayu1254/AnyKernel3.git; then
+		echo -e "\nCloning AnyKernel3 repo failed! Aborting..."
+		exit 1
+	fi
+	cp $kernel $dtbo AnyKernel3
+	cp $dtb AnyKernel3/dtb
+	rm -f *zip
+	cd AnyKernel3 || exit
+	rm -rf out/arch/arm64/boot
+	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+	cd ..
+	rm -rf AnyKernel3
+	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+	echo "Zip: $ZIPNAME"
+	curl --upload-file "$ZIPNAME" https://free.keep.sh
+	echo
+else
+	echo -e "\nCompilation failed!"
+fi
